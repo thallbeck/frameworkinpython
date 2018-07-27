cc553f783f1f7b2c764a263c7bfcb8ff36fcec50
thallbeck <timhallbeck@yahoo.com>
2018-07-27 00:47:05 -0600

    added some methods to the General class

--- framework/general.py
+++ framework/general.py
@@ -1,18 +1,57 @@
 import inspect
 
 class General:
-    silentMode = False
-    detailedDebugMode = False
-    initialStackDepth = 1
+    silent_mode = False
+    detailed_debug_mode = False
+    initial_stack_depth = 1
     DATA_PATH = '/Users/Public/Documents/testautomation/'
 
     def __init__(self):
-        self.initialStackDepth = len(inspect.stack())
+        self.initial_stack_depth = self.get_stack_depth()
 
     def set_silent_mode(self, mode):
-        prev = mode
-        silentMode = mode
+        prev = self.silent_mode
+        self.silent_mode = mode
         return prev
+
+    def get_silent_mode(self):
+        return self.silent_mode
+
+    def set_detailed_debug_mode(self, mode):
+        prev = self.detailed_debug_mode
+        self.detailed_debug_mode = mode
+        return prev
+
+    def get_detailed_debug_mode(self):
+        return self.detailed_debug_mode
+
+    def get_stack_depth(self):
+        return len(inspect.stack())
+
+    def debug(self, string_list, add_bullet_info):
+        if (self.get_silent_mode()):
+            return False
+
+        if type(string_list) is str:
+            print(string_list)
+        else:
+            for string in string_list:
+                print(string)
+        return True
+
+
+
+
+
+
+    def run_unit_tests(self):
+        self.set_silent_mode(self.get_silent_mode())
+        self.set_detailed_debug_mode(self.get_detailed_debug_mode())
+        self.get_stack_depth()
+        self.debug('single string test', False)
+        self.debug(('multiple', 'string', 'test'), False)
+
+
 
 
 
--- framework/page.py
+++ framework/page.py
@@ -4,10 +4,18 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import general
 
 universal = general.General()
+universal.run_unit_tests()
 universal.set_silent_mode(False)
+universal.debug('\nstarting the test', False)
+
+some_strings = ('string1', 'string2', 'string3')
+universal.debug(some_strings, False)
+
 binary = FirefoxBinary(r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
 driver = webdriver.Firefox(firefox_binary=binary, executable_path='C:\\Users\\Public\\Documents\\testautomation\\geckodriver.exe')
 driver.get("http://www.facebook.com")
 assert "Facebook" in driver.title
 driver.quit()
+
+universal.debug('\nending the test', False)
 
